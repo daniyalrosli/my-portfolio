b1f0e9ee66ea5ef57013876a004eb26cec1ef42b
daniyalrosli <94339715+daniyalrosli@users.noreply.github.com>
2025-03-09 14:36:49 +0800

    update ui

--- src/app/experiences/page.tsx
+++ src/app/experiences/page.tsx
@@ -1,6 +1,7 @@
 // app/experience/page.tsx
 import Navbar from "../components/navbar"; // Ensure this path is correct
 import { FaCheckCircle } from "react-icons/fa"; // Import icon for bullet points
+import Image from "next/image";
 
 interface Experience {
   role: string;
@@ -8,6 +9,7 @@ interface Experience {
   location: string;
   period: string;
   isRemote: boolean;
+  logo: string;
   points?: string[];
 }
 
@@ -18,6 +20,7 @@ const experiences: Experience[] = [
     location: "Penang, Malaysia",
     period: "Upcoming (March 2025 — September 2025)",
     isRemote: false,
+    logo: "/img/amd.png",
     points: ["Will be coming"],
   },
   {
@@ -26,6 +29,7 @@ const experiences: Experience[] = [
     location: "Malaysia",
     period: "2023 — 2023",
     isRemote: true,
+    logo: "/img/gdsc.jpeg",
     points: [
       "Responsible for frontend development for Kitahack 2024's website with other team members",
       "Utilized Angular for the front-end development",
@@ -39,6 +43,7 @@ const experiences: Experience[] = [
     location: "Bayan Lepas, Pulau Pinang",
     period: "2022 — 2023",
     isRemote: false,
+    logo: "/img/da.jpeg",
     points: [
       "Managed multiple tasks efficiently, including creating visually engaging content using DaVinci Resolve, Adobe Premiere Pro, Canva, and Adobe Photoshop",
       "Collaborated within a team environment, contributing to web design projects using Elementor and WordPress",
@@ -65,11 +70,20 @@ const ExperiencePage = () => {
               className="bg-gray-800/70 rounded-3xl shadow-lg p-8 hover:shadow-xl transition-all duration-300 border border-gray-700 transform hover:-translate-y-1 hover:scale-[1.02]"
             >
               <div className="flex flex-col sm:flex-row justify-between gap-4 mb-5">
-                <div>
-                  <h2 className="text-2xl font-semibold text-white">
-                    {exp.role}
-                  </h2>
-                  <p className="text-lg text-gray-400 mt-1">{exp.company}</p>
+                <div className="flex items-center gap-4">
+                  <Image
+                    src={exp.logo}
+                    alt={`${exp.company} logo`}
+                    width={50}
+                    height={50}
+                    className="rounded-lg"
+                  />
+                  <div>
+                    <h2 className="text-2xl font-semibold text-white">
+                      {exp.role}
+                    </h2>
+                    <p className="text-lg text-gray-400 mt-1">{exp.company}</p>
+                  </div>
                 </div>
 
                 <div className="sm:text-right">
