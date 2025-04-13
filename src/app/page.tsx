0fdd2b96187accbf1f345d9a785e2815a9d7302a
daniyalrosli <94339715+daniyalrosli@users.noreply.github.com>
2025-04-13 23:13:17 +0800

    update img

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -147,7 +147,7 @@ export default function HomePage() {
             {/* Image */}
             <div className="relative w-full h-full rounded-full border-2 border-blue-500/30 p-1">
               <Image
-                src="/img/m.jpeg"
+                src="/img/t.jpeg"
                 alt="Daniyal Rosli"
                 fill
                 className="rounded-full object-cover shadow-lg"
